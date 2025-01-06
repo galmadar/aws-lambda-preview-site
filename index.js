5c651deaee73bc6e8d3b94cf7c2a24515ae4f490
Gal Madar <madargal@gmail.com>
2025-01-07 01:09:50 +0200

    1

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ exports.handler = async (event) => {
     //     statusCode: 200,
     //     body: JSON.stringify({ message: 'Hello World' }),
     // };
-    // //console.log({event});
+    console.log({event});
 
     const url = event?.url;
 
